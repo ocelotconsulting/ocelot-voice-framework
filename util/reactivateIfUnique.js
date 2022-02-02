ea0493c9425e515952edea9fd66ebfcd207f8252
lucasbarth <lucasrbarth@gmail.com>
2022-02-02 11:55:15 -0600

    debug log

--- util/reactivateIfUnique.js
+++ util/reactivateIfUnique.js
@@ -1,5 +1,7 @@
 const reactivateIfUnique = ({ currentSubConversation, conversationStack, conversationSet }) => {
+  console.log('first', JSON.stringify({ currentSubConversation, conversationStack, conversationSet }))
   if (conversationSet[Object.keys(currentSubConversation)[0]].shouldBeUnique) {
+    console.log('second', JSON.stringify({ currentSubConversation, conversationStack, conversationSet }))
     let index = conversationStack.flatMap(conversation => Object.keys(conversation)).indexOf(Object.keys(currentSubConversation)[0])
 
     if (index > -1) {
@@ -16,6 +18,7 @@ const reactivateIfUnique = ({ currentSubConversation, conversationStack, convers
       const moveArray = conversationStack.splice(startIndex, (endIndex - startIndex) + 1)
       currentSubConversation = moveArray.splice(moveArray.length - 1, 1)[0]
       conversationStack.splice(conversationStack.length, 0, ...moveArray)
+      console.log('third', JSON.stringify({ currentSubConversation, conversationStack, conversationSet }))
     }
   }
 

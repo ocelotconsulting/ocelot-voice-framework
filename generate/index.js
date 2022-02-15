3465174aaa8939a55425339b629c5dce49190008
lucasbarth <lucasrbarth@gmail.com>
2022-02-15 16:29:42 -0600

    not ideal

--- generate/index.js
+++ generate/index.js
@@ -3,14 +3,9 @@ const createHandler = require('./createHandler')
 const createLocalizationInterceptor = require('./createLocalizationInterceptor')
 const ErrorHandler = require('./ErrorHandler')
 
-module.exports = ({ conversationSet, fetchSession, saveSession, dialogs }) => {
-  const StateHandler = createHandler({ conversationSet, fetchSession, saveSession })
-  const DialogInterceptor = createLocalizationInterceptor(dialogs)
-
-  return Alexa.SkillBuilders
-    .custom()
-    .addRequestHandlers(StateHandler)
-    .addRequestInterceptors(DialogInterceptor)
-    .addErrorHandlers(ErrorHandler)
-    .lambda()
-}
+module.exports = ({ conversationSet, fetchSession, saveSession, dialogs }) => Alexa.SkillBuilders
+  .custom()
+  .addRequestHandlers(createHandler({ conversationSet, fetchSession, saveSession }))
+  .addRequestInterceptors(createLocalizationInterceptor(dialogs))
+  .addErrorHandlers(ErrorHandler)
+  .lambda()

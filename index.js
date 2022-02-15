a83dd784e97350c955e998e558d73b7c5ded1cf7
lucasbarth <lucasrbarth@gmail.com>
2022-02-15 16:03:52 -0600

    trying

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+const Alexa = require('ask-sdk-core')
+const ErrorHandler = require('./generate/ErrorHandler')
+
 module.exports = {
   generate: require('./generate'),
+  generateApp: ({ StateHandler, DialogInterceptor }) => Alexa.SkillBuilders
+    .custom()
+    .addRequestHandlers(StateHandler)
+    .addRequestInterceptors(DialogInterceptor)
+    .addErrorHandlers(ErrorHandler)
+    .lambda(),
   utils: require('./utils'),
   conversationTemplates: require('./conversationTemplates'),
 }

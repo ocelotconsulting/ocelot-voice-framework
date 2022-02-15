832f249fb663530923150c68238bcdf21ec8a242
lucasbarth <lucasrbarth@gmail.com>
2022-02-15 16:13:03 -0600

    try again

--- generate/index.js
+++ generate/index.js
@@ -1,9 +1,16 @@
+
 const createHandler = require('./createHandler')
 const createLocalizationInterceptor = require('./createLocalizationInterceptor')
 const ErrorHandler = require('./ErrorHandler')
 
-module.exports = ({ conversationSet, fetchSession, saveSession, dialogs }) => ({
-  StateHandler: createHandler({ conversationSet, fetchSession, saveSession }),
-  DialogInterceptor: createLocalizationInterceptor(dialogs),
-  ErrorHandler,
-})
+module.exports = ({ conversationSet, fetchSession, saveSession, dialogs }) => {
+  const StateHandler = createHandler({ conversationSet, fetchSession, saveSession })
+  const DialogInterceptor = createLocalizationInterceptor(dialogs)
+
+  return Alexa.SkillBuilders
+    .custom()
+    .addRequestHandlers(StateHandler)
+    .addRequestInterceptors(DialogInterceptor)
+    .addErrorHandlers(ErrorHandler)
+    .lambda()
+}

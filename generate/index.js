ff51ab1ec52620db97e0337063899bf4305f0345
lucasbarth <lucasrbarth@gmail.com>
2022-02-15 15:30:11 -0600

    nope

--- generate/index.js
+++ generate/index.js
@@ -1,16 +1,9 @@
-const Alexa = require('ask-sdk-core')
 const createHandler = require('./createHandler')
 const createLocalizationInterceptor = require('./createLocalizationInterceptor')
 const ErrorHandler = require('./ErrorHandler')
 
-module.exports = ({ conversationSet, fetchSession, saveSession, dialogs }) => {
-  const StateHandler = createHandler({ conversationSet, fetchSession, saveSession })
-  const DialogInterceptor = createLocalizationInterceptor(dialogs)
-
-  return Alexa.SkillBuilders
-    .custom()
-    .addRequestHandlers(StateHandler)
-    .addRequestInterceptors(DialogInterceptor)
-    .addErrorHandlers(ErrorHandler)
-    .lambda()
-}
+module.exports = ({ conversationSet, fetchSession, saveSession, dialogs }) => ({
+  StateHandler: createHandler({ conversationSet, fetchSession, saveSession }),
+  DialogInterceptor: createLocalizationInterceptor(dialogs),
+  ErrorHandler,
+})

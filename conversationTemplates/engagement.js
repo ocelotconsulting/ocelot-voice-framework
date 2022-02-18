589bc7109315cfe24253e63dce13852740299d4c
lucasbarth <lucasrbarth@gmail.com>
2022-02-18 15:52:55 -0600

    bug fix

--- conversationTemplates/engagement.js
+++ conversationTemplates/engagement.js
@@ -7,19 +7,39 @@ module.exports = ({
 }) => ({
   handle: () => ({
     stateMap: (transitionsMap => {
-      const allTransitions = state(...Object.keys(transitionsMap).map(key => transition('processIntent', key, guard((ctx, { intent }) => typeof transitionsMap[key].intent === 'string' ? intent?.name === transitionsMap[key].intent : transitionsMap[key].intent.includes(intent?.name)))))
+      const allTransitions = state(
+        ...Object.keys(transitionsMap).map(key => transition(
+          'processIntent',
+          key,
+          guard((ctx, { intent }) => typeof transitionsMap[key].intent === 'string' ?
+            intent?.name === transitionsMap[key].intent :
+            transitionsMap[key].intent.includes(intent?.name)
+          ),
+        ))
+      )
 
-      return Object.keys(transitionsMap).reduce((acc, transition) => ({ ...acc, [transition]: allTransitions }), { fresh: allTransitions, resume: allTransitions })
+      return Object.keys(transitionsMap).reduce((acc, transition) => ({
+        ...acc,
+        [transition]: allTransitions,
+      }), {
+        fresh: allTransitions,
+        resume: allTransitions,
+      })
     })(transitions),
     transitionStates: Object.keys(transitions),
-    interceptCallback: ({ conversationStack, currentSubConversation, intent }) => Object.keys(transitions.filter(transition => transition.canInterrupt)).reduce((acc, transitionType) => ({ ...acc, [transitionType]: intent => {
-        if (intent?.name === transitions[transitionType].intent) {
-          conversationStack.push(currentSubConversation)
-          currentSubConversation = { [transitionType]: {}}
-        }
+    interceptCallback: ({ conversationStack, currentSubConversation, intent }) => Object.keys(transitions)
+      .filter(({ canInterrupt }) => canInterrupt)
+      .reduce((acc, transitionType) => ({
+        ...acc,
+        [transitionType]: intent => {
+          if (intent?.name === transitions[transitionType].intent) {
+            conversationStack.push(currentSubConversation)
+            currentSubConversation = { [transitionType]: {}}
+          }
 
-        return { conversationStack, currentSubConversation }
-      }}, {}))[currentSubConversation[Object.keys(currentSubConversation)[0]]](intent),
+          return { conversationStack, currentSubConversation }
+        },
+      }, {}))[currentSubConversation[Object.keys(currentSubConversation)[0]]](intent),
     dialogMap: {
       fresh: greetingDialog,
       resume: reEngageDialog,
